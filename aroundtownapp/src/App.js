dff98c72f21782c66005b16efb2d9a9c4d432944
Darius Faison <33269709+DariusFaison@users.noreply.github.com>
2021-11-08 14:36:03 -0500

    Got basic sign in done with minimal styling

--- aroundtownapp/src/App.js
+++ aroundtownapp/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './App.css';
+import logo from './resources/around-town-logo.png'
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => (
+    <div>
+        <header>
+            <img class="sign-in-logo" src={logo}/>
+        </header>
+        <AmplifySignOut />
+        <div class="textstuff">
+            This will change things.
+        </div>
     </div>
-  );
-}
+);
 
-export default App;
+export default withAuthenticator(App);
