9cd580a3cfd1285f721a9fe1f50b3ea0691f5fca
Darius Faison <33269709+DariusFaison@users.noreply.github.com>
2021-11-08 15:53:28 -0500

    Added a footer with the mission statement and sample text

--- aroundtownapp/src/App.js
+++ aroundtownapp/src/App.js
@@ -11,12 +11,21 @@ Amplify.configure(awsconfig);
 const App = () => (
     <div>
         <header>
-            <img class="sign-in-logo" src={logo}/>
+            <img class="sign-in-logo" src={logo} />
         </header>
         <AmplifySignOut />
-        <div class="textstuff">
-            This will change things.
+
+        <div class="wrapper">
+            <div class="container">
+                <input type="text" class="input" placeholder="AroundTown" />
+                    <input type="button" value="Search" class="close-btn" />
+            </div>   
         </div>
+
+        <div class="textstuff">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </div>
+            <footer> Where local businesses get national exposure, and great customers receive reputable service! </footer>
     </div>
 );
 
