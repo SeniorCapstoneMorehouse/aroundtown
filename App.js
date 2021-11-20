d24ea69dd863a652124417dbd351317216abd966
Guy-JM <91451083+Guy-JM@users.noreply.github.com>
2021-11-20 01:24:16 -0500

    added a map
    to the home page

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 //import React from "react";
 // APP JS with more HOMEPAGE CSS DESIGN
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+//import { useEffect, useRef } from "react";
 //import ReactMapGL, { NavigationControl } from "react-map-gl";
 import logo from "./karlout.regular (1).png";
-import map from "./map.jpeg";
+import mappic from "./map.jpeg";
 import NearBy from "./karlout.regular.Near.png";
+import amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
 import "./App.css";
 import "./login.js";
+import Map from "./Map.js";
 import {
   withAuthenticator,
   AmplifySignOut,
   AmplifySignIn,
   AmplifyAuthenticator,
 } from "@aws-amplify/ui-react";
+import { createMap } from "maplibre-gl-js-amplify";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import { drawPoints } from "maplibre-gl-js-amplify";
+import { Geo } from "aws-amplify";
+amplify.configure(awsconfig);
+
+//* ---- CONSTANTS ----- */
+
+const INITIAL_VIEWPORT = {
+  longitude: -56.164532,
+  latitude: -34.901112,
+};
 
 function App() {
+  const [map, setMap] = useState();
+  useEffect(() => {
+    const initializeMap = async () => {
+      const map = await createMap({
+        container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
+        center: [INITIAL_VIEWPORT.longitude, INITIAL_VIEWPORT.latitude],
+        zoom: 13,
+      });
+      //map.addControl(createAmplifyGeocoder());
+
+      setMap(map);
+      //const stores = getStores();
+      //addStoreLocations(map, stores);
+    };
+
+    initializeMap();
+
+    return function cleanup() {
+      console.log("remove");
+      map.remove();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header></header>
@@ -62,11 +102,12 @@ function App() {
               <a href="">Pure Atlanta</a>
             </li>
           </ul>
-          <img className="map" src={map} alt="near by map" width="300" />
+          <img className="map" src={mappic} alt="near by map" width="300" />
         </div>
         <div className="logout">
           <AmplifySignOut />
         </div>
+        <Map myMap={map} />
       </AmplifyAuthenticator>
     </div>
   );
